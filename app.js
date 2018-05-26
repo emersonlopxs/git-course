e2e35b6c9886258fb6851242833de1fb82226e41
lupuselit <lupuselit@outlook.com>
2018-05-26 11:12:03 -0300

    add some code

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 console.log('hello world!');
 
 // this should be a new branch!
+
+if (true) {
+  // do something
+}
+
+while (true) {
+  // do something
+}
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
