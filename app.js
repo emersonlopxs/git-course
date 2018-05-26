a9993ad0126829c16d4a056b3db03485fb89293d
lupuselit <lupuselit@outlook.com>
2018-05-26 13:02:55 -0300

    delete all those functions

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
 console.log('hello world!');
 
-console.log('this is the new world!');
-
-const lastName = 'Lopes';
-
-// this should be a new branch!
-
-if (true) {
-  // do something
-}
-
-while (true) {
-  // do something
-}
-
-for (var i = 0; i < array.length; i++) {
-  array[i]
-}
-
-// this line shouldn't exist
-function add(x,y) {
-  return x + y;
-}
-
-function sub(x,y) {
-  return x + y;
-}
-
-const revert = {
-  title: 'this should be reverted'
-}
-
 const name = 'Emerson';
